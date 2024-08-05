ac4737079f0c72d3466f989bf2edb778ed1355af
oromadin <osromadin@gmail.com>
2024-08-05 21:50:40 +0000

    simple table

--- index.php
+++ index.php
@@ -4,6 +4,36 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 ?>
 <body>
-<h1>future table</h1>
-  <?php \JoliCode\PhpOsHelper\OsHelper::isWork(); ?>
+  <table style="border:1px solid black;">
+    <tr>
+      <th style="border:1px solid black">Static Method</th>
+      <th style="border:1px solid black">Result</th>
+    </tr>
+    <tr>
+      <td align = "center" style="border:1px solid black;">isUnix()</td>
+      <td align = "center" style="border:1px solid black;">1</td>
+    </tr>
+    <tr>
+      <td align = "center" style="border:1px solid black;">isWindowsSubsystemForLinux()</td>
+      <td align = "center" style="border:1px solid black;">2</td>
+    </tr>
+    <tr>
+      <td align = "center" style="border:1px solid black;">isWindows()</td>
+      <td align = "center" style="border:1px solid black;">3</td>
+    </tr>
+    <tr>
+      <td align = "center" style="border:1px solid black;">isWindowsSeven()</td>
+      <td align = "center" style="border:1px solid black;">4</td>
+    </tr>
+    <tr>
+      <td align = "center" style="border:1px solid black;">isWindowsEightOrHigher()</td>
+      <td align = "center" style="border:1px solid black;">5</td>
+    </tr>
+    <tr>
+      <td align = "center" style="border:1px solid black;">isMacOS()</td>
+      <td align = "center" style="border:1px solid black;">6</td>
+    </tr>
+  </table>
 </body>
+
+<?php \JoliCode\PhpOsHelper\OsHelper::isWork(); ?>
